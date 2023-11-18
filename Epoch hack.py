22c34d5bf45edb688516586fd52e487a6a71c3f0
Richie-Lee <73502276+Richie-Lee@users.noreply.github.com>
2023-11-18 11:54:23 +0100

    Initial commit

---
+++ Epoch hack.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+# Replace 'your_file.csv' with the path to your CSV file
+df = pd.read_csv("C:/Users/RLee/Downloads/epoch-hackathon-2023/train.csv")
+
+
+# In[3]:
+
+
+df.head(10)
+
+
+# In[4]:
+
+
+len(df)
+
+
+# In[6]:
+
+
+df.describe()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
